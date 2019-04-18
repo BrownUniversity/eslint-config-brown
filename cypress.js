c86c0aefa4213e62f0907546f050f4ff0cae88c9
Colin Cummings <colin_cummings@brown.edu>
2019-04-18 11:35:52 -0400

    add and configure eslint-plugin-chai-friendly for cypress extension

--- cypress.js
+++ cypress.js
@@ -1,7 +1,11 @@
 // https://github.com/cypress-io/eslint-plugin-cypress
 module.exports = {
   extends: ["plugin:cypress/recommended"],
-  plugins: ["cypress"],
+  plugins: ["cypress", "chai-friendly"],
+  rules: {
+    "no-unused-expressions": 0,
+    "chai-friendly/no-unused-expressions": 2
+  },
   env: {
     "cypress/globals": true
   }
